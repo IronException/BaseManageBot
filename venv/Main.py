784a54adf6fe66ce2fc88d95c8e39e640234d267
IronException <mail-fuer-friedolin@web.de>
2019-12-16 00:28:27 +0100

    cant check author yet

--- venv/Main.py
+++ venv/Main.py
@@ -8,10 +8,10 @@ class MyClient(discord.Client):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        global reaction
         print('Message from {0.author}: {0.content}'.format(message))
-
-        reaction = reaction.react_to_discord_message(message)
+        if message.author.id == "IronException#4092":
+            print("but yea..")
+            await message.channel.send('Send me that \N{THUMBS UP SIGN} reaction, mate')
 
 
 client = MyClient()
